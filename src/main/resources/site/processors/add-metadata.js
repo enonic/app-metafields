642940772458545f96674d48c71b7d1da84f0e8d
Bruno Reis <reisfmb@gmail.com>
2023-10-09 23:07:03 +0200

    Added support for XHTML self closing tags #107

--- src/main/resources/site/processors/add-metadata.js
+++ src/main/resources/site/processors/add-metadata.js
@@ -4,6 +4,9 @@ const libs = {
     metadata: require("/lib/metadata")
 };
 
+const HTML_MEDIA_TYPE = 'text/html';
+const XML_MEDIA_TYPES = ['application/xhtml+xml', 'application/xml', 'text/xml'];
+
 exports.responseProcessor = function (req, res) {
     const reusableData = libs.metadata.getReusableData();
     const site = reusableData.site;
@@ -11,58 +14,52 @@ exports.responseProcessor = function (req, res) {
     const siteConfig = reusableData.siteConfig;
 
     // Avoid page processor if the app is set to headless mode
-    if (siteConfig.headless) return res;
-
-    const metadata = libs.metadata.getMetaData(site, siteConfig, content, "html") || "";
-
-    // Force arrays since single values will be return as string instead of array
-    res.pageContributions.headEnd = libs.util.data.forceArray(res.pageContributions.headEnd);
-
-    // Push metadata
-    res.pageContributions.headEnd.push(metadata);
-
-    // Handle injection of title - use any existing tag by replacing its content.
-    // Also - Locate the <html> tag and make sure the "og" namespace is added.
-    const titleHtml = libs.metadata.getTitle(site, content) || "";
+    if ( siteConfig.headless ) {
+        return res;
+    }
 
     let titleAdded = false;
+    const isResponseContentTypeHtml = res.contentType.indexOf(HTML_MEDIA_TYPE) > -1;
+    const isResponseContentTypeXml = XML_MEDIA_TYPES.some(xmlMediaType => res.contentType.indexOf(xmlMediaType) > -1);
 
-    if (
-        res.contentType.indexOf("text/html") > -1 &&
-        res.body &&
-        typeof res.body === "string"
-    ) {
-        // Find a title in the html and use that instead of adding our own title
+    if ( isResponseContentTypeHtml && res.body && typeof res.body === "string" ) {
         const titleHasIndex = res.body.indexOf("<title>") > -1;
         const htmlIndex = res.body.toLowerCase().indexOf("<html");
         const endHtmlIndex = res.body.indexOf(">", htmlIndex);
 
+        // Handle injection of title - use any existing tag by replacing its content.
         // Svg are text/html can have a <title>
         if (titleHasIndex && htmlIndex > -1) {
-            res.body = res.body.replace(/(<title>)(.*?)(<\/title>)/i, titleHtml); //REPLACE ALL TITLES!
+            const titleHtml = libs.metadata.getTitle(site, content) || "";
+            res.body = res.body.replace(/(<title>)(.*?)(<\/title>)/i, titleHtml);
             titleAdded = true;
         }
 
-        // Find <html> and if it does not have proper "og"-prefix - inject it!
+        // Locate the <html> tag and make sure the "og" namespace is added.
         if (htmlIndex >= 0 && endHtmlIndex >= 0) {
             const fixedHtmlTagInnerContent = libs.metadata.getFixedHtmlAttrsAsString(res.body);
-
-            res.body = 
-                res.body.substr(0, htmlIndex + 5) +
-                " " +
-                fixedHtmlTagInnerContent +
-                res.body.substr(endHtmlIndex);
+            res.body = res.body.substr(0, htmlIndex + 5) + " " + fixedHtmlTagInnerContent + res.body.substr(endHtmlIndex);
         }
     }
 
-    if (!titleAdded) {
+    // Force arrays since single values will be return as string instead of array
+    res.pageContributions.headEnd = libs.util.data.forceArray(res.pageContributions.headEnd);
+
+    // Push metadata if response content type is html or xml
+    if ( isResponseContentTypeHtml || isResponseContentTypeXml ) {
+        const selfClosingTags = isResponseContentTypeXml;
+        const metadata = libs.metadata.getMetaData(site, siteConfig, content, "html", selfClosingTags) || "";
+        res.pageContributions.headEnd.push(metadata);
+    }
+
+    if ( !titleAdded ) {
+        const titleHtml = libs.metadata.getTitle(site, content) || "";
         res.pageContributions.headEnd.push(titleHtml);
     }
 
-    if (req.params) {
-        if (req.params.debug === "true") {
-            res.applyFilters = false; // Skip other filters
-        }
+    // Skip other filters
+    if ( req.params && req.params.debug === "true" ) {
+        res.applyFilters = false; 
     }
 
     return res;
