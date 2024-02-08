bf91848334785b52a560093f62fdb87033d82362
Christian Westgaard <comlock@gmail.com>
2024-02-08 12:54:49 +0100

    Separate type

--- src/main/resources/lib/common/getAppendix.ts
+++ src/main/resources/lib/common/getAppendix.ts
@@ -5,18 +5,21 @@ import type {MetafieldsSiteConfig} from '/lib/types/MetafieldsSiteConfig';
 import {getTheConfig} from '/lib/common/getTheConfig';
 
 
+interface GetAppendixParams {
+	applicationConfig: Record<string, string|boolean>
+	applicationKey: string
+	isFrontpage?: boolean
+	site: Site<MetafieldsSiteConfig>
+}
+
+
 // Concat site title? Trigger if set to true in settings, or if not set at all (default = true)
 export const getAppendix = ({
 	applicationConfig, // Avoid app.config so it can be used in Guillotine Extension Context
 	applicationKey, // Avoid app.name so it can be used in Guillotine Extension Context
 	isFrontpage,
 	site,
-}: {
-	applicationConfig: Record<string, string|boolean>
-	applicationKey: string
-	isFrontpage?: boolean
-	site: Site<MetafieldsSiteConfig>
-}) => {
+}: GetAppendixParams): string => {
 	const siteConfig = getTheConfig({
 		applicationConfig,
 		applicationKey,
