53a2f97faf2642c123aa05371e9fbc0dd0f1f66d
Christian Westgaard <comlock@gmail.com>
2024-04-29 16:02:12 +0200

    Fix #164 Error on a page rendered via controller mapping

--- src/main/resources/lib/app-metafields/constants.ts
+++ src/main/resources/lib/app-metafields/constants.ts
@@ -3,4 +3,5 @@ import {getDashedAppName} from '/lib/app-metafields/xp/getDashedAppName';
 
 export const APP_NAME_PATH = getDashedAppName(); // Outside Guillotine Extension Context
 export const DEBUG = false;
+export const TRACE = false;
 export const MIXIN_PATH = 'meta-data';
